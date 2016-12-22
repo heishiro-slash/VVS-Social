bcd037fc59ca0ce888494c0fc50fbc145380d73c
José Miguel <jm.riocortegoso@outlook.es>
2016-12-22 22:05:23 +0100

    Fix Travis 2

--- test/joinme/modelo/muro/GestorMuroTest.java
+++ test/joinme/modelo/muro/GestorMuroTest.java
@@ -172,4 +172,75 @@ public class GestorMuroTest {
         assertTrue(v.equals(e.getVisibilidad()));
     }
     
+    /*Debido a falta de tiempo no ha sido posible implementar este test generado con GraphWalker
+    $ gw3 offline -m Login.graphml "random(edge_coverage(100))"*/
+    @Test
+    public void TestGraphWalker(){/*
+    {"currentElementName":"e_init"}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_validUser"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_register"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_incorrectData"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_correctData"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_register"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_correctData"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_validUser"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_correctPublishData"}
+{"currentElementName":"v_PublishState"}
+
+     */}
+    
+ 
+    
 }
