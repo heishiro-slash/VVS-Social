66f45106754471f72d5547e0ace089b13ee83e8d
José Miguel <jm.riocortegoso@gmail.com>
2016-12-22 21:58:42 +0100

    GraphWalker

---
+++ test/joinme/graphwalker/TestGraphWalker.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joinme.graphwalker;
+
+/**
+ *
+ * @author jmrio
+ */
+public class TestGraphWalker {
+    /*Debido a falta de tiempo no ha sido posible implementar este test generado con GraphWalker
+    $ gw3 offline -m Login.graphml "random(edge_coverage(100))"*/
+
+ /*
+    {"currentElementName":"e_init"}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_validUser"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_register"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_incorrectData"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_correctData"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_incorrectUser"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_register"}
+{"currentElementName":"v_RegisterUser"}
+{"currentElementName":"e_correctData"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_incorrectPublishData"}
+{"currentElementName":"v_PublishState"}
+{}
+{"currentElementName":"v_UserNotInSession"}
+{"currentElementName":"e_login"}
+{"currentElementName":"v_LoginUser"}
+{"currentElementName":"e_validUser"}
+{"currentElementName":"v_ClientInSession"}
+{"currentElementName":"e_publish"}
+{"currentElementName":"v_PublishState"}
+{"currentElementName":"e_correctPublishData"}
+{"currentElementName":"v_PublishState"}
+
+     */
+}
